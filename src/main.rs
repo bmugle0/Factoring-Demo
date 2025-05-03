063f887ca974638299aaa782a69e0220e7cc36c3
Brendon M <bmugle0@gmail.com>
2025-05-02 22:27:11 -0700

    I finished the mult-threading of the project.

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,9 @@ use std::sync::mpsc;
 use std::thread;
 
 fn main() {
-    let num: i64 = -500;
-    let (tx, rx) = mpsc::channel();
+    let num: i64 = -9000000000000000000;
     
-    let tx1 = tx.clone();
-    
-    thread::spawn(move || {
-        tx1.send(find_factors(num, 1..=250)).unwrap();
-    });
-    
-    thread::spawn(move || {
-        tx.send(find_factors(num, 251..=500)).unwrap();
-    });
-    
-    let result: Vec<(i64, i64)> = append_results(rx);
-    
+    let result = start_threads(num, 10);
     println!("{:?}", result);
 }
 
@@ -24,6 +12,7 @@ fn find_factors(num: i64, range: std::ops::RangeInclusive<i64>) -> Vec<(i64, i64
     let mut result: Vec<(i64, i64)> = Vec::new();
     
     for i in range {
+        //println!("i={i}");
         if i > (num.abs()/i) {
             return result;
         }
@@ -35,11 +24,36 @@ fn find_factors(num: i64, range: std::ops::RangeInclusive<i64>) -> Vec<(i64, i64
     result
 }
 
+fn start_threads(num: i64, threads: i64) -> Vec<(i64, i64)>{    
+    let mut parts: Vec<i64> = vec![];
+    parts.append(&mut (0..=threads.abs())
+        .into_iter()
+        .map(|i| i * ((num.abs().isqrt()+3)/threads))
+        .collect::<Vec<i64>>());
+        println!("{:?}", parts);
+        
+    let (tx, rx) = mpsc::channel();
+    
+    for i in 0..=threads-2 {
+        start_single_thread(num, parts[i as usize]+1, parts[(i+1) as usize], tx.clone());
+    }
+    start_single_thread(num, parts[(threads-1) as usize]+1, parts[threads as usize], tx);
+        
+    append_results(rx)
+}
+
+fn start_single_thread(num: i64, start: i64, end: i64, tx: mpsc::Sender<Vec<(i64, i64)>>) {  
+    thread::spawn(move || {
+    tx.send(find_factors(num, start..=end)).unwrap();
+    });
+}
+
 fn append_results(rx: mpsc::Receiver<Vec<(i64, i64)>>) -> Vec<(i64, i64)> {
     let mut result: Vec<(i64, i64)> = Vec::new();
     for mut received in rx {
         result.append(&mut received);
     }
-    
+
+    result.sort_by_key(|(f1,_)| *f1);
     result
 }
